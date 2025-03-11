4ed655dd22d02b4aaa5819a55a1601dd1299a539
jeongbeomSeo <jeongbeom4693@gmail.com>
2025-03-11 21:16:42 +0900

    [ISSUE-#9] - AuthController의 signup() 실패 테스트 작성 (RED)

--- src/test/java/com/eventty/eventtynextgen/auth/controller/AuthControllerTest.java
+++ src/test/java/com/eventty/eventtynextgen/auth/controller/AuthControllerTest.java
@@ -12,14 +12,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.eventty.eventtynextgen.auth.model.UserRole;
 import com.eventty.eventtynextgen.auth.model.dto.request.SignupRequest;
 import com.eventty.eventtynextgen.auth.service.AuthService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 import org.hamcrest.Matcher;
 import org.hamcrest.core.StringEndsWith;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -63,6 +69,37 @@ class AuthControllerTest {
                 .andExpect(header().string("Location", StringEndsWith.endsWith("/api/v1/auth/100")));
 
             verify(authService, times(1)).signup(any(SignupRequest.class));
+        }
+
+        @ParameterizedTest
+        @MethodSource("invalidSignupRequest")
+        @DisplayName("회원 등록 실패 - 입력 데이터 유효성 검증")
+        void 회원등록_실패_입력데이터_유효성_검증_실패(String email, String password, String phone, String birth, UserRole userRole) throws Exception {
+            // given
+            SignupRequest signupRequest = new SignupRequest(email, password, phone, birth,
+                userRole);
+            Long createdId = 100L;
+
+            // when
+            ResultActions resultActions = mockMvc.perform(
+                post("/api/v1/auth")
+                    .content(objectMapper.writeValueAsString(signupRequest))
+                    .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            // then
+            resultActions.andExpect(status().is4xxClientError());
+        }
+
+        private static Stream<Arguments> invalidSignupRequest() {
+            return Stream.of(
+                Arguments.of("email", "12345678", "000-0000-0000", "1999-01-01", UserRole.USER),
+                Arguments.of("email@mm.mm", "1234", "000-0000-0000", "1999-01-01", UserRole.USER),
+                Arguments.of("email@mm.mm", "12345678", "0000-0000", "1999-01-01", UserRole.USER),
+                Arguments.of("email@mm.mm", "12345678", "000-0000-0000", "199-01", UserRole.USER),
+                Arguments.of("email@mm.mm", "12345678", "000-0000-0000", "1999-99-99", UserRole.USER),
+                Arguments.of("email@mm.mm", "12345678", "000-0000-0000", "1999-01-01", null)
+            );
         }
     }
 
