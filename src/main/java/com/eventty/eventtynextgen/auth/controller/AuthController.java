734fb2c5e61de9afd148d55622bd50cc4e35e4b8
jeongbeomSeo <jeongbeom4693@gmail.com>
2025-03-24 21:16:41 +0900

    [ISSUE-#9] - authController에서 '/email-check' API의 URL 수정: 누락되어 있는 BASE_PATH 추가 - authController에 deleteUser API 추가

--- src/main/java/com/eventty/eventtynextgen/auth/controller/AuthController.java
+++ src/main/java/com/eventty/eventtynextgen/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,7 +36,7 @@ public class AuthController {
         return ResponseEntity.created(location).build();
     }
 
-    @GetMapping("email-check")
+    @GetMapping(BASE_PATH + "/email-check")
     public ResponseEntity<Boolean> checkEmail(@RequestParam(value = "email") String email) {
 
         boolean result = authService.checkEmail(email);
@@ -43,7 +44,13 @@ public class AuthController {
         return ResponseEntity.ok(result);
     }
 
-    // TODO: 회원 삭제 (SOFT)
+    @DeleteMapping(BASE_PATH)
+    public ResponseEntity<Void> deleteUser(@RequestParam(value = "auth-id") Long authUserId) {
+
+        authService.delete(authUserId);
+
+        return ResponseEntity.ok().build();
+    }
 
     // TODO: 이메일 검증
 }
