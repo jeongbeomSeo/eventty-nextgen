5c4ecd023ddedbdb390c8ab31f60fb8ba4f55e70
jeongbeomSeo <jeongbeom4693@gmail.com>
2025-05-01 20:13:32 +0900

    [ISSUE-#28] - exsists 조회 방식을 findBy 방식으로 수정

--- src/main/java/com/eventty/eventtynextgen/user/repository/UserRepository.java
+++ src/main/java/com/eventty/eventtynextgen/user/repository/UserRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByEmail(String email);
-
     Optional<User> findByEmail(String email);
 
     List<User> findByNameAndPhone(String name, String phone);
