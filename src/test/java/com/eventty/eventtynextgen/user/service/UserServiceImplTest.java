48bc551929f1c2a0f47625eaac6ca4a2d51f92d2
jeongbeomSeo <jeongbeom4693@gmail.com>
2025-05-01 21:06:57 +0900

    Revert "[ISSUE-#18] (#27)"
    This reverts commit 85965dbd19c0c93346bd32828101ad7f92b57d68.

--- src/test/java/com/eventty/eventtynextgen/user/service/UserServiceImplTest.java
+++ src/test/java/com/eventty/eventtynextgen/user/service/UserServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.eventty.eventtynextgen.user;
+package com.eventty.eventtynextgen.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
