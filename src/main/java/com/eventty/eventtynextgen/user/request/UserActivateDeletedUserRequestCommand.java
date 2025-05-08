d34d406756ffbe714247d99cf2178ddb8ed202b5
jeongbeomSeo <jeongbeom4693@gmail.com>
2025-05-08 17:36:27 +0900

    [ISSUE-#31] - 불필요한 UserActivateDeletedUserRequestCommand 삭제

--- src/main/java/com/eventty/eventtynextgen/user/request/UserActivateDeletedUserRequestCommand.java
+++
@@ -1,9 +0,0 @@
-package com.eventty.eventtynextgen.user.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record UserActivateDeletedUserRequestCommand(
-    @NotNull(message = "userId는 필수값 입니다.")
-    Long userId
-) {
-}
