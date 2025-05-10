e01cbdfe62ddde2fd99a274147668b8fcf67443c
jeongbeomSeo <jeongbeom4693@gmail.com>
2025-05-10 16:11:25 +0900

    [#29] PasswordMatcher 검증 방식 수정 (#34)
    * [ISSUE-#29]
    - PasswordMathValidator 방식에서 AOP 방식으로 전환
    - 하나의 매개변수의 두 필드를 확인한다는 행위에 초점을 맞춰서 FieldMatch 어노테이션 및 FieldMatchAspect로 네이밍
    - 리플랙션 기반의 두 필드의 값 검사 로직 구현
    
    * [ISSUE-#29]
    - Objects.equals를 사용하여 두 객체 비교 로직을 깔끔하게 정돈
    
    * [ISSUE-#29]
    - UserChnagePasswordResponseCommand에서 updatedPasswordConfirm 필드 삭제
      - updatedPassword와 updatedPasswordConfirm 비교 로직 제거
      - 해당 로직은 클라이언트(프론트) 비즈니스 로직에 포함되어야 한다.
      - API 스펙 변경으로 인한 통합 테스트 케이스 수정

--- src/main/java/com/eventty/eventtynextgen/user/request/UserChangePasswordRequestCommand.java
+++ src/main/java/com/eventty/eventtynextgen/user/request/UserChangePasswordRequestCommand.java
@@ -1,10 +1,8 @@
 package com.eventty.eventtynextgen.user.request;
 
-import com.eventty.eventtynextgen.user.shared.annotation.PasswordMatch;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
-@PasswordMatch
 public record UserChangePasswordRequestCommand(
 
     @NotNull(message = "id값은 필수값입니다.")
@@ -12,8 +10,6 @@ public record UserChangePasswordRequestCommand(
     @Length(min = 8, max = 16, message = "패스워드는 8자 이상 16자 이하여야 합니다.")
     String currentPassword,
     @Length(min = 8, max = 16, message = "패스워드는 8자 이상 16자 이하여야 합니다.")
-    String updatedPassword,
-    @Length(min = 8, max = 16, message = "패스워드는 8자 이상 16자 이하여야 합니다.")
-    String updatedPasswordConfirm
+    String updatedPassword
 ){
 }
